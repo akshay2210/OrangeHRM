9b25833d956de2911093b3a6a8140b286a93ffa1
Akshay <105150085+akshay2210@users.noreply.github.com>
2022-05-09 15:57:36 +0530

    it is our first commit

---
+++ src/test/java/com/TestLayer/AdminPageTest.java
@@ -0,0 +1,10 @@
+package com.TestLayer;
+
+public class AdminPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/TestLayer/PIMPageTest.java
@@ -0,0 +1,10 @@
+package com.TestLayer;
+
+public class PIMPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
